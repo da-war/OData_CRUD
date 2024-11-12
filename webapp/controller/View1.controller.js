a767e5848f6b2a8670e8d5bad67cfef98eb4f54b
Rana Dawar Abdullah <reactnativekadena@gmail.com>
2024-11-12 08:33:10 +0000

    bug removed

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,12 +1,26 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller"
 ],
-function (Controller) {
-    "use strict";
+    function (Controller) {
+        "use strict";
 
-    return Controller.extend("odatacrud.controller.View1", {
-        onInit: function () {
+        return Controller.extend("odatacrud.controller.View1", {
+            onInit: function () {
+                this.onReadAll();
+            },
+            onReadAll: function () {
+                const that = this;
+                const oModel = this.getOwnerComponent().getModel();
+                oModel.read("/Products", {
+                    success: function (odata) {
+                        console.log(odata);
+                        const jModel = new sap.ui.model.json.JSONModel(odata);
+                        that.getView().byId("idProducts").setModel(jModel);
 
-        }
+                    }, error: function (onError) {
+                        console.log(onError)
+                    }
+                });
+            }
+        });
     });
-});
