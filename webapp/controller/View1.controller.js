0d83546b0b7f65b27ea1ae3615aed10ad138804c
Rana Dawar Abdullah <reactnativekadena@gmail.com>
2024-11-12 16:21:27 +0000

    done

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -6,7 +6,7 @@ sap.ui.define([
 
         return Controller.extend("odatacrud.controller.View1", {
             onInit: function () {
-                this.oReadKey();
+                // this.oReadAll();
             },
             oReadAll: function () {
                 const that = this;
@@ -16,7 +16,6 @@ sap.ui.define([
                         console.log(odata);
                         const jModel = new sap.ui.model.json.JSONModel(odata);
                         that.getView().byId("idProducts").setModel(jModel);
-
                     }, error: function (onError) {
                         console.log(onError)
                     }
@@ -83,6 +82,69 @@ sap.ui.define([
                         console.log(error)
                     }
                 })
+            },
+            oEdit: function (oEvent) {
+                const that = this;
+                const oModel = this.getOwnerComponent().getModel();
+                oModel.setUseBatch(false);
+                if (oEvent.getSource().getText() === "Edit") {
+                    oEvent.getSource().setText("Submit");
+                    oEvent.getSource().getParent().getParent().getCells()[3].setEditable(true);
+                }
+                else {
+
+                    oEvent.getSource().setText("Edit")
+                    oEvent.getSource().getParent().getParent().getCells()[3].setEditable(false);
+
+                    const oInput = oEvent.getSource().getParent().getParent().getCells()[3].getValue();
+                    const oId = oEvent.getSource().getBindingContext().getProperty("ID");
+
+                    oModel.update("/Products(" + oId + ")", { Rating: oInput }, {
+                        success: function (odata) {
+                            console.log(odata);
+                            that.oReadAll();
+                        },
+                        error: function (error) {
+                            console.log(error)
+                        }
+                    })
+                }
+
+            },
+            oDuplicate: function (oEvent) {
+                const that = this;
+                const oModel = this.getOwnerComponent().getModel();
+                oModel.setUseBatch(false);
+
+                const oDuplicateData = oEvent.getSource().getBindingContext().getObject();
+                oDuplicateData.ID = 100 + oDuplicateData.ID;
+                oModel.create("/Products", oDuplicateData, {
+                    success: function (odata) {
+                        console.log(odata);
+                        that.oReadAll();
+                    },
+                    error: function (error) {
+                        console.log(error)
+                    }
+                })
+
+            },
+            oDelete: function (oEvent) {
+                const that = this;
+                const oModel = this.getOwnerComponent().getModel();
+                oModel.setUseBatch(false);
+
+                const oId = oEvent.getSource().getBindingContext().getProperty("ID");
+                oModel.remove("/Products(" + oId + ")", {
+                    success: function (odata) {
+                        console.log(odata);
+                        that.oReadAll();
+                    },
+                    error: function (error) {
+                        console.log(error)
+                    }
+                })
+
             }
         });
     });
