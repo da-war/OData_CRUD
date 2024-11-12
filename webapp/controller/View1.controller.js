8e2e3acd5fcee110bfe8c1213f605b5f486bb8ce
Rana Dawar Abdullah <reactnativekadena@gmail.com>
2024-11-12 13:20:29 +0000

    All possible functions to perform for search from db

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -6,9 +6,9 @@ sap.ui.define([
 
         return Controller.extend("odatacrud.controller.View1", {
             onInit: function () {
-                this.onReadAll();
+                this.oReadKey();
             },
-            onReadAll: function () {
+            oReadAll: function () {
                 const that = this;
                 const oModel = this.getOwnerComponent().getModel();
                 oModel.read("/Products", {
@@ -21,6 +21,68 @@ sap.ui.define([
                         console.log(onError)
                     }
                 });
+            },
+            oReadFilters: function () {
+                const that = this;
+                const oModel = this.getOwnerComponent().getModel();
+                const oFilter = new sap.ui.model.Filter("Rating", "EQ", "4");
+                oModel.read("/Products", {
+                    filters: [oFilter],
+                    success: function (odata) {
+                        console.log(odata);
+                        const jModel = new sap.ui.model.json.JSONModel(odata);
+                        that.getView().byId("idProducts").setModel(jModel);
+                    },
+                    error: function (error) {
+                        console.log(error)
+                    }
+                })
+
+            },
+            oReadSorter: function () {
+                const that = this;
+                const oModel = this.getOwnerComponent().getModel();
+                const oSorter = new sap.ui.model.Sorter("Name", false);
+                oModel.read("/Products", {
+                    sorters: [oSorter],
+                    success: function (odata) {
+                        console.log(odata);
+                        const jModel = new sap.ui.model.json.JSONModel(odata);
+                        that.getView().byId("idProducts").setModel(jModel);
+                    },
+                    error: function (error) {
+                        console.log(error)
+                    }
+                })
+            },
+            oReadParameters: function () {
+                const that = this;
+                const oModel = this.getOwnerComponent().getModel();
+                oModel.read("/Products", {
+                    urlParameters: { $skip: 0, $top: 4 },
+                    success: function (odata) {
+                        console.log(odata);
+                        const jModel = new sap.ui.model.json.JSONModel(odata);
+                        that.getView().byId("idProducts").setModel(jModel);
+                    },
+                    error: function (error) {
+                        console.log(error)
+                    }
+                })
+            },
+            oReadKey: function () {
+                const that = this;
+                const oModel = this.getOwnerComponent().getModel();
+                oModel.read("/Products(3)", {
+                    success: function (odata) {
+                        console.log(odata);
+                        const jModel = new sap.ui.model.json.JSONModel({ results: [odata] });
+                        that.getView().byId("idProducts").setModel(jModel);
+                    },
+                    error: function (error) {
+                        console.log(error)
+                    }
+                })
             }
         });
     });
